e9034482f7becd8c299d7f1c4727ee466818525d
unknown <hadeerraouf4@gmail.com>
2024-03-17 14:42:28 +0200

    custom nav bar .

--- brandy_vue/src/App.vue
+++ brandy_vue/src/App.vue
@@ -1,30 +1,63 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="wrapper">
+    <nav class="navbar navbar-expand-lg navbar-light  fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"><strong>Brandy</strong></a>
+        <button class="navbar-toggler" type="button" 
+        data-bs-toggle="collapse" 
+        data-bs-target="#main" 
+        aria-controls="main"
+         aria-expanded="false"
+         aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+          <!-- <span><i class="fa-solid fa-bars"></i></span> -->
+        </button>
+        <div class="collapse navbar-collapse" id="main">
+          <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+             
+                <router-link to="/" class="nav-link p-lg-2">Summer</router-link> 
+                
+             
+              <!-- <a class="nav-link active" aria-current="page" href="#">Home</a> -->
+            </li>
+            <li class="nav-item">
+              <router-link to="/" class="nav-link p-lg-2">Winter</router-link>
+            </li>
+
+            <li class="nav-item">
+              <router-link to="/" class="btn btn-light  p-lg-2">Log in</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/" class="btn btn-success p-lg-2 ms-2">
+                <span class="icon"> <i class="fas fa-shopping-cart"></i></span>
+                <span>Cart</span>
+              </router-link>
+            </li>
+          </ul>
+          <!-- <form class="d-flex">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form> -->
+        </div>
+      </div>
+    </nav>
+    <section class="section">
+      <router-view/>
+    </section>
+    <footer class="footer">
+      <p class="text-center">Copyright (c) 2024</p>
+    </footer>
+    <!-- <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav> -->
+  </div>
+  
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
