2f7dc076f6ea7ab80b7ae9cd7042b884b5bf6593
unknown <hadeerraouf4@gmail.com>
2024-06-23 16:14:26 +0300

    modify cart page .

--- brandy_vue/src/components/CartItem.vue
+++ brandy_vue/src/components/CartItem.vue
@@ -1,12 +1,24 @@
 <template>
-<tr>
+    <tr>
+        <td>
+            <RouterLink :to="item.product.get_absolute_url">{{item.product.name}}</RouterLink>
+        </td>
+        <td>{{item.product.price}}</td>
+        <td>{{item.quantity}}
+            <a @click="decrementQuantity(item)">-</a>
+            <a @click="increamentQuantity(item)">+</a>
+        </td>
+        <td>${{getItemTotal(item).toFixed(2)}}</td>
+        <td><button  type="button" class="btn btn-danger text-decoration-none" @click="removeFromCart(item)">Delete</button></td>
+    </tr>
+<!-- <tr>
     <td>
         <RouterLink :to="item.product.get_absolute_url">{{item.product.name}}</RouterLink>
     </td>
     <td>{{item.quantity}}</td>
     <td>${{getItemTotal(item).toFixed(2)}}</td>
     <td><button  type="button" class="btn btn-danger"></button></td>
-</tr>
+</tr> -->
 </template>
 <script>
 export default {
@@ -23,6 +35,24 @@ export default {
         getItemTotal(item){
             return item.quantity * item.price
         }
-    }
+    },
+    decrementQuantity(item){
+        item.quantity -=1
+        if(item.quantity === 0 ){
+            this.$emit('removeFromCart',item)
+        }
+        this.updateCart()
+
+    },
+increamentQuantity(item){
+    item.quantity +=1
+    this.updateCart()
+},
+updateCart(){
+    localStorage.setItem('cart',JSON.stringify(this.$store.state.cart))
+},removeFromCart(item){
+    this.$emit('removeFromCart',item)
+    this.updateCart()
+}
 }
 </script>
--- brandy_vue/src/views/Cart.vue
+++ brandy_vue/src/views/Cart.vue
@@ -1,89 +1,128 @@
 <template>
-<div class="page-cart">
+  <div class="page-cart">
     <div class="row m-0">
-        <div class="col-12">
-            <h1 class="title">
-                Cart
-            </h1>
-            <CartItem></CartItem>
-            <table class="table table-shadow" v-if="cartTotalLength">
-                <thead>
-                  <tr>
-                    <th scope="col">Product</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">quantity</th>
-                    <th scope="col">Total</th>
-                    <th scope="col">control</th>
-                  </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="item in cart.items" 
-                    :key="item.product.id">
-                        <td>
-                            <RouterLink :to="item.product.get_absolute_url">{{item.product.name}}</RouterLink>
-                        </td>
-                        <td>{{item.product.price}}</td>
-                        <td>{{item.quantity}}</td>
-                        <td>${{getItemTotal(item).toFixed(2)}}</td>
-                        <td><button  type="button" class="btn btn-danger text-decoration-none">Delete</button></td>
-                    </tr>
-                </tbody>
-            </table>
-            <p v-else>You don't have any products in your cart...</p>
+      <div class="col-12">
+        <h1 class="title">Cart</h1>
+        <CartItem></CartItem>
+        <table class="table table-shadow" v-if="cartTotalLength">
+          <thead>
+            <tr>
+              <th scope="col">Product</th>
+              <th scope="col">Price</th>
+              <th scope="col">quantity</th>
+              <th scope="col">Total</th>
+              <th scope="col">control</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="item in cart.items" :key="item.product.id">
+              <td>
+                <RouterLink :to="item.product.get_absolute_url">{{
+                  item.product.name
+                }}</RouterLink>
+              </td>
+              <td>{{ item.product.price }}</td>
+              <td>
+                {{ item.quantity }}
+                <a @click="decrementQuantity(item)">-</a>
+                <a @click="increamentQuantity(item)">+</a>
+              </td>
+              <td>${{ getItemTotal(item).toFixed(2) }}</td>
+              <td>
+                <button
+                  type="button"
+                  class="btn btn-danger text-decoration-none"
+                  @click="removeFromCart(item)"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <p v-else>You don't have any products in your cart...</p>
+      </div>
+      <div class="col-12">
+        <div class="card w-100 table-shadow custome-card" style="width: 18rem">
+          <div class="card-body">
+            <h5 class="card-title">Summary</h5>
+            <p class="card-text">
+              <strong>${{ cartTotalPrice.toFixed(2) }}</strong
+              >,{{ cartTotalLength }} items
+            </p>
+            <hr />
+            <router-link to="/cart/checkout" class="btn btn-dark"
+              >Process to checkout</router-link
+            >
+          </div>
         </div>
-        <div class="col-12">
-            <div class="card w-100 table-shadow custome-card" style="width: 18rem;">
-                <div class="card-body">
-                  <h5 class="card-title">Summary</h5>
-                  <p class="card-text"> <strong>${{cartTotalPrice.toFixed(2)}}</strong>,{{ cartTotalLength }} items </p>
-                  <router-link to="/cart/checkout" class="btn btn-dark ">Process to checkout</router-link>
-                </div>
-              </div>
-            <!-- <h2>Summary</h2>
+        <!-- <h2>Summary</h2>
             <strong>${{cartTotalPrice.toFixed(2)}}</strong>,{{ cartTotalLength }} items
             <hr>
             <router-link to="/cart/checkout" clas="btn btn-dark">Process to checkout</router-link> -->
-        </div>
-
+      </div>
     </div>
-</div>
-
+  </div>
 </template>
 <script>
-import axios from 'axios';
+import axios from "axios";
 // import CartItem from '@/components/CartItem'
 export default {
-    name:'Cart',
-    component :{
-        // CartItem
+  name: "Cart",
+  component: {
+    // CartItem
+  },
+  data() {
+    return {
+      cart: {
+        items: [],
+      },
+    };
+  },
+  mounted() {
+    this.cart.items = this.$store.state.cart.items;
+    console.log("cart", this.cart.items);
+  },
+  computed: {
+    cartTotalLength() {
+      return this.cart.items.reduce((acc, curVal) => {
+        return (acc += curVal.quantity);
+      }, 0);
     },
-    data(){
-        return{
-            cart:{
-                items : []
-            }
-        }
+    cartTotalPrice() {
+      return this.cart.items.reduce((acc, curVal) => {
+        return (acc += curVal.product.price * curVal.quantity);
+      }, 0);
     },
-    mounted(){
-        this.cart.items = this.$store.state.cart.items
-        console.log("cart",this.cart.items)
+  },
+  methods: {
+    getItemTotal(item) {
+      return item.quantity * item.product.price;
     },
-    computed:{
-        cartTotalLength(){
-            return this.cart.items.reduce((acc, curVal) =>{
-                return acc += curVal.quantity
-            },0)
-        },
-        cartTotalPrice(){
-            return this.cart.items.reduce((acc, curVal)=>{
-                return acc += curVal.product.price * curVal.quantity
-            },0)
-        }
+
+    decrementQuantity(item) {
+      console.log("decrement");
+      item.quantity -= 1;
+      if (item.quantity === 0) {
+        this.removeFromCart(item);
+      }
+      this.updateCart();
     },
-    methods:{
-        getItemTotal(item){
-            return item.quantity * item.product.price
-        }
-    }
-}
+    increamentQuantity(item) {
+      console.log("increamentQuantity");
+      item.quantity += 1;
+      this.updateCart();
+    },
+    updateCart() {
+      localStorage.setItem("cart", JSON.stringify(this.$store.state.cart));
+    },
+    removeFromCart(item) {
+        console.log("delete function")
+      this.cart.items = this.cart.items.filter
+        (i => i.product.id !== item.product.id
+      );
+      this.updateCart();
+    },
+  },
+};
 </script>
